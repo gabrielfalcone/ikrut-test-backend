553de7c438314821c499d4005885766abb39c4c2
Gabriel Falcone <gabrielfalcone@gmail.com>
2016-12-21 10:01:10 -0300

    first commit

---
+++ backend/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using backend.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace backend.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class LoginController : ApiController
+    {
+        public async Task<IHttpActionResult> Post([FromBody] PostLoginViewModel model)
+        {
+            ApplicationUser user = await UserManager.FindAsync(model.username, model.password);
+            if (user == null)
+                return NotFound();
+            return Ok(user.Email);
+        }
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+    }
+}
